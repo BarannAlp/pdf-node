e76e42e793440e1b1678c2aeca233353c82409eb
Baran <126600817+BarannAlp@users.noreply.github.com>
2024-10-01 01:51:24 +0300

    start

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const errorHandler = require("./middlewares/errorHandler");
 const app = express();
 //! Connect to mongodb
 mongoose
-  .connect("mongodb://localhost:27017/auth-api")
+  .connect("mongodb+srv://barannalp:barannalp@cluster0.rkumy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/auth-api")
   .then(() => console.log("Db connected successfully"))
   .catch((e) => console.log(e));
 
