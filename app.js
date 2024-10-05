179840397560ffc17dbf350978b65ca680660aa4
Baran <126600817+BarannAlp@users.noreply.github.com>
2024-10-05 12:17:50 +0300

    cors

--- app.js
+++ app.js
@@ -8,10 +8,12 @@ const cors = require('cors');
 
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'https://pdf-node-seven.vercel.app'], // Allow both local and your deployed frontend
+  origin: ['http://localhost:3000', 'https://pdf-node-seven.vercel.app', 'https://yurtcim-talimat-yonetim.vercel.app'], // Add the correct origin
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+  credentials: true, // If you are using cookies or authentication, enable credentials
 };
+
 // Use CORS with the options
 const app = express();
 app.use(cors(corsOptions));
