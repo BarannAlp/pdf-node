d84080b72b847860a1597d6d39f60500be37bc6f
Baran <126600817+BarannAlp@users.noreply.github.com>
2024-10-09 12:29:06 +0300

    yeni db

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 
 //! Connect to mongodb
 mongoose
-  .connect("mongodb+srv://barannalp:barannalp@cluster0.rkumy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/auth-api")
+  .connect("mongodb+srv://yurtcim:yurtcim@yurtcim.vuqrf.mongodb.net/")
   .then(() => console.log("Db connected successfully"))
   .catch((e) => console.log(e));
   const multer = require("multer");
